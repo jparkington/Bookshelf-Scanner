8bd774b100142c4405ba2b84d6eeafcc8db58755
DevAkre <59783143+DevAkre@users.noreply.github.com>
2024-12-09 01:44:04 -0500

    Bounding boxes

--- bookshelf_scanner/frontend/app.py
+++ bookshelf_scanner/frontend/app.py
@@ -1,19 +1,30 @@
 import streamlit as st  
 import random
-from PIL import Image, ImageOps
+from PIL import Image, ImageOps, ImageDraw
 import numpy as np
 from bookshelf_scanner import BookSegmenter
 
 st.set_page_config(page_title="Bookshelf Scanner")
-segmenter = BookSegmenter()
-st.title("Bookshelf Scanner")
-file = st.sidebar.file_uploader(type=["jpg", "png"], label="Upload an image")
-        
-if file is None:
-    print("No file uploaded")
-else:
-    image = Image.open(file)
-    image = ImageOps.exif_transpose(image)
-    books, bboxes, confidences = segmenter(np.array(image))
-    st.image(image, use_container_width=True)
-    
+
+def draw_bounding_boxes(image, bboxes):
+    draw = ImageDraw.Draw(image)
+    for bbox in bboxes:
+        draw.rectangle(bbox, outline="red", width=3)
+    return image
+
+def main():
+    segmenter = BookSegmenter()
+    st.title("Bookshelf Scanner")
+    file = st.sidebar.file_uploader(type=["jpg", "png"], label="Upload an image")
+
+    if file is None:
+        print("No file uploaded")
+    else:
+        image = Image.open(file)
+        image = ImageOps.exif_transpose(image)
+        books, bboxes, confidences = segmenter(np.array(image))
+        image_with_boxes = draw_bounding_boxes(image, bboxes)
+        st.image(image_with_boxes, use_container_width=True)
+
+if __name__ == "__main__":
+    main()
