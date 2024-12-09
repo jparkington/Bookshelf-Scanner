366123cbaf9f471fc68d09dc246f5dd173112b62
DevAkre <59783143+DevAkre@users.noreply.github.com>
2024-12-09 01:37:12 -0500

    Update app.py

--- bookshelf_scanner/frontend/app.py
+++ bookshelf_scanner/frontend/app.py
@@ -4,15 +4,16 @@ from PIL import Image, ImageOps
 import numpy as np
 from bookshelf_scanner import BookSegmenter
 
-st.set_page_config(layout="wide", page_title="Bookshelf Scanner")
+st.set_page_config(page_title="Bookshelf Scanner")
+segmenter = BookSegmenter()
 st.title("Bookshelf Scanner")
-file = st.file_uploader(type=["jpg", "png"], label="Upload an image")
+file = st.sidebar.file_uploader(type=["jpg", "png"], label="Upload an image")
         
 if file is None:
     print("No file uploaded")
 else:
     image = Image.open(file)
     image = ImageOps.exif_transpose(image)
-
+    books, bboxes, confidences = segmenter(np.array(image))
     st.image(image, use_container_width=True)
     
